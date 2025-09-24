3099dfc5fd6cf842b2462349e118a42f45ea3dd5
framirezj <12649259+framirezj@users.noreply.github.com>
2025-09-24 19:33:46 -0300

    feat(crypto): crea endpoint para obtener el total del portafolio

--- routes/crypto.route.js
+++ routes/crypto.route.js
@@ -6,6 +6,19 @@ router.get('/', (req, res) => {
     res.status(200).json({message: 'Hola desde Crypto'})
 })
 
+router.post('/', async (req, res) => {
+    try {
+        
+        res.json({monto_fake: "$muuuuucho dinero"})
+        
+    } catch (error) {
+        console.error("Error al obtener el total del portafolio", error)
+        res.status(500).json({error_message: "No se pudo obtener la información"})
+    }
+    
+    
+})
+
 
 export default router
 
